08ecb6b2ad95baff2c21bab86549f44398060e6f
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-10-15 07:21:59 -0500

    add container and chart

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import ElasticAPM from 'elastic-apm-node';
+
+const apm = ElasticAPM.start();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
